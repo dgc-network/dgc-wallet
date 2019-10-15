37b76e68c5813c0fbc6294ea7ae44f0ea80ed1da
roverchen <rover.k.chen@gmail.com>
2019-10-15 13:29:32 +0800

    '%d',

--- includes/class-dgc-wallet-wallet.php
+++ includes/class-dgc-wallet-wallet.php
@@ -222,7 +222,7 @@ if ( ! class_exists( 'dgc_Wallet_Wallet' ) ) {
                     'details' => $details, 
                     'deleted' => 0, 
                     'date' => time() 
-                )) ;
+                ), array( '%d', '%d', '%d', '%s', '%f', '%f', '%s', '%s', '%d', '%d') ) ;
                 $dgc_API_args = array(
                     'table'		=> $wpdb->prefix . 'dgc_wallet_transactions',
                     'data'		=> $data,
