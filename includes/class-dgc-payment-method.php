5a2f755ab8e0bb88680805a2562e1620e33ceb5f
roverchen <rover.k.chen@gmail.com>
2021-02-24 22:26:28 +0800

    /** 	 * Register the JavaScript and stylesheets for the admin area. 	 * 	 * @since    1.0.0 	 */

--- includes/class-dgc-payment-method.php
+++ includes/class-dgc-payment-method.php
@@ -140,7 +140,7 @@ class dgc_Payment_Method extends WC_Payment_Gateway {
             return;
         }
         //$payment_response = dgc_payment()->payment->debit( get_current_user_id(), $order->get_total( 'edit' ), apply_filters('dgc_payment_order_payment_description', __( 'For order payment #', 'text-domain' ) . $order->get_order_number(), $order) );
-        if ( $payment_response = dgc_payment()->payment->debit( get_current_user_id(), $order->get_total( 'edit' ), apply_filters('dgc_payment_order_payment_description', __( 'Paid to order #', 'text-domain' ) . $order->get_order_number(), $order) ) ) {
+        if ( $payment_response = dgc_payment()->payment->debit( get_current_user_id(), $order->get_total( 'edit' ), apply_filters('dgc_payment_order_payment_description', __( 'Paid for order #', 'text-domain' ) . $order->get_order_number(), $order) ) ) {
             foreach ( $order->get_items() as $item_id => $item ) {
                 $product_id = $item->get_product_id();
                 $total = $item->get_total();
