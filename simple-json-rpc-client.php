31d1bbc5ad2146da1ab6e9e68f11f5c8ccc10888
roverchen <rover.k.chen@gmail.com>
2021-03-16 14:24:51 +0800

    getinfo

--- simple-json-rpc-client.php
+++ simple-json-rpc-client.php
@@ -6,13 +6,17 @@ use SimpleJsonRpcClient\Request\Request;
 use SimpleJsonRpcClient\Exception\BaseException;
 use SimpleJsonRpcClient\Response\Response;
 
-// Initialize the client. Credentials are optional.
-//$client = new Client('localhost', 'username', 'password');
-$client = new Client('165.232.130.97:7998', 'DiGitalCoin', 'dIgITALcOIN');
+add_shortcode( 'getinfo', 'json_rpc_shortcode' );
 
-try {
-	$request = new Request('getinfo');
-	$response = $client->sendRequest($request);
+function json_rpc_shortcode() {
+    // Initialize the client. Credentials are optional.
+    //$client = new Client('localhost', 'username', 'password');
+    $client = new Client('165.232.130.97:7998', 'DiGitalCoin', 'dIgITALcOIN');
+
+    try {
+	    $request = new Request('getinfo');
+	    $response = $client->sendRequest($request);
+        return $response;
 /*
 	// Send a request without parameters. The "id" will be added automatically unless supplied.
 	// Request objects return their JSON representation when treated as strings.
@@ -33,7 +37,8 @@ try {
 	$request = new Request('method', null, 2);
 	$response = $client->sendRequest($request);
 */    
-}
-catch (BaseException $e) {
-	echo $e->getMessage();
+    }
+    catch (BaseException $e) {
+	    echo $e->getMessage();
+    }
 }
