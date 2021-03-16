acfda6908f29789e635110e6b297e0c4d9982225
roverchen <rover.k.chen@gmail.com>
2021-03-16 16:50:35 +0800

    $block = $bitcoind->getBlock

--- SimpleJsonRpcClient/simple-json-rpc-client.php
+++ SimpleJsonRpcClient/simple-json-rpc-client.php
@@ -4,15 +4,16 @@
  * Don't forget to include composer autoloader by uncommenting line below
  * if you're not already done it anywhere else in your project.
  **/
-/*
+
 use Denpa\Bitcoin\Client as BitcoinClient;
 
 function bitcoin_client_shortcode() {
 	$bitcoind = new BitcoinClient('http://DiGitalCoin:dIgITALcOIN@165.232.130.97:7998/');
-
+	$info = $bitcoind->getinfo();
+	echo $info;
 }
 add_shortcode( 'dgc-getinfo', 'bitcoin_client_shortcode' );
-*/
+
 
 use SimpleJsonRpcClient\Client\HttpPostClient as Client;
 
@@ -55,5 +56,5 @@ function json_rpc_shortcode() {
 	    echo $e->getMessage();
     }
 }
-add_shortcode( 'dgc-getinfo', 'json_rpc_shortcode' );
+//add_shortcode( 'dgc-getinfo', 'json_rpc_shortcode' );
 
