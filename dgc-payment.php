85296f1111032cb9cb6fde3220760bcf1b8259b0
roverchen <rover.k.chen@gmail.com>
2021-03-16 15:34:55 +0800

    SimpleJsonRpcClient

--- dgc-payment.php
+++ dgc-payment.php
@@ -30,7 +30,7 @@ if ( ! defined( 'DGC_PAYMENT_PLUGIN_FILE' ) ) {
 /*
  * Include JsonRpc file.
  */
-include_once dirname( __FILE__ ) . 'simple-json-rpc-client.php';
+include_once dirname( __FILE__ ) . '/SimpleJsonRpcClient/simple-json-rpc-client.php';
 if ( ! class_exists( 'JsonRpc' ) ){
     //include_once dirname( __FILE__ ) . '/SimpleJsonRpcClient/json-rpc.php';
 	//$client = new JsonRpc( 'http://[SERVER]/json_rpc_server.php' );
