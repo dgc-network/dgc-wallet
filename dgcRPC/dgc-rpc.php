ebf1533aa8db55e2f6fcd133f07027ac0f4cdff8
roverchen <rover.k.chen@gmail.com>
2021-03-17 20:43:19 +0800

    getblockchaininfo

--- dgcRPC/dgc-rpc.php
+++ dgcRPC/dgc-rpc.php
@@ -70,25 +70,3 @@ function wporg_shortcodes_init() {
 }
  
 add_action( 'init', 'wporg_shortcodes_init' );
-
-/**
- * Another Example.
- */
-
-function recent_posts_function($atts){
-	extract(shortcode_atts(array(
-	   'posts' => 1,
-	), $atts));
- 
-	$return_string = '<ul>';
-	query_posts(array('orderby' => 'date', 'order' => 'DESC' , 'showposts' => $posts));
-	if (have_posts()) :
-	   while (have_posts()) : the_post();
-		  $return_string .= '<li><a href="'.get_permalink().'">'.get_the_title().'</a></li>';
-	   endwhile;
-	endif;
-	$return_string .= '</ul>';
- 
-	wp_reset_query();
-	return $return_string;
- }
