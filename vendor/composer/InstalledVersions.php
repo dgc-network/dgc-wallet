82a2e49362aa55a3ab5b8840a8018812b3907e37
roverchen <rover.k.chen@gmail.com>
2021-03-31 17:25:47 +0800

    digitalcoin

--- vendor/composer/InstalledVersions.php
+++ vendor/composer/InstalledVersions.php
@@ -30,7 +30,7 @@ private static $installed = array (
     'aliases' => 
     array (
     ),
-    'reference' => 'ba183fd9e1d45cdcf1b6c6b70c221ecb18b89622',
+    'reference' => '7c26937fc82c072c4122e26ab0a7144771399e51',
     'name' => 'dgc-network/dgc-payment',
   ),
   'versions' => 
@@ -44,15 +44,6 @@ private static $installed = array (
       ),
       'reference' => '1a0357fccad9d1cc1ea0c9a05b8847fbccccb78d',
     ),
-    'denpa/php-bitcoinrpc' => 
-    array (
-      'pretty_version' => 'v2.1.3',
-      'version' => '2.1.3.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '578dab84e713225b91105c54ee3485a078285788',
-    ),
     'dgc-network/dgc-payment' => 
     array (
       'pretty_version' => 'dev-master',
@@ -60,75 +51,16 @@ private static $installed = array (
       'aliases' => 
       array (
       ),
-      'reference' => 'ba183fd9e1d45cdcf1b6c6b70c221ecb18b89622',
+      'reference' => '7c26937fc82c072c4122e26ab0a7144771399e51',
     ),
-    'guzzlehttp/guzzle' => 
+    'psr/log' => 
     array (
-      'pretty_version' => '7.3.0',
-      'version' => '7.3.0.0',
+      'pretty_version' => '1.1.3',
+      'version' => '1.1.3.0',
       'aliases' => 
       array (
       ),
-      'reference' => '7008573787b430c1c1f650e3722d9bba59967628',
-    ),
-    'guzzlehttp/promises' => 
-    array (
-      'pretty_version' => '1.4.1',
-      'version' => '1.4.1.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '8e7d04f1f6450fef59366c399cfad4b9383aa30d',
-    ),
-    'guzzlehttp/psr7' => 
-    array (
-      'pretty_version' => '1.8.1',
-      'version' => '1.8.1.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '35ea11d335fd638b5882ff1725228b3d35496ab1',
-    ),
-    'psr/http-client' => 
-    array (
-      'pretty_version' => '1.0.1',
-      'version' => '1.0.1.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '2dfb5f6c5eff0e91e20e913f8c5452ed95b86621',
-    ),
-    'psr/http-client-implementation' => 
-    array (
-      'provided' => 
-      array (
-        0 => '1.0',
-      ),
-    ),
-    'psr/http-message' => 
-    array (
-      'pretty_version' => '1.0.1',
-      'version' => '1.0.1.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => 'f6561bf28d520154e4b0ec72be95418abe6d9363',
-    ),
-    'psr/http-message-implementation' => 
-    array (
-      'provided' => 
-      array (
-        0 => '1.0',
-      ),
-    ),
-    'ralouphie/getallheaders' => 
-    array (
-      'pretty_version' => '3.0.3',
-      'version' => '3.0.3.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '120b605dfeb996808c31b6477290a714d356e822',
+      'reference' => '0f73288fd15629204f9d42b7055f72dacbe811fc',
     ),
     'roundcube/plugin-installer' => 
     array (
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,29 +6,10 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit77eafbbc4e8d8d8e07e585a9c1feff8c
 {
-    public static $files = array (
-        '7b11c4dc42b3b3023073cb14e519683c' => __DIR__ . '/..' . '/ralouphie/getallheaders/src/getallheaders.php',
-        'c964ee0ededf28c96ebd9db5099ef910' => __DIR__ . '/..' . '/guzzlehttp/promises/src/functions_include.php',
-        'a0edc8309cc5e1d60e3047b5df6b7052' => __DIR__ . '/..' . '/guzzlehttp/psr7/src/functions_include.php',
-        '37a3dc5111fe8f707ab4c132ef1dbc62' => __DIR__ . '/..' . '/guzzlehttp/guzzle/src/functions_include.php',
-        'ef3d9dab4f9e36ea1b6959f291959656' => __DIR__ . '/..' . '/denpa/php-bitcoinrpc/src/functions.php',
-    );
-
     public static $prefixLengthsPsr4 = array (
         'P' => 
         array (
-            'Psr\\Http\\Message\\' => 17,
-            'Psr\\Http\\Client\\' => 16,
-        ),
-        'G' => 
-        array (
-            'GuzzleHttp\\Psr7\\' => 16,
-            'GuzzleHttp\\Promise\\' => 19,
-            'GuzzleHttp\\' => 11,
-        ),
-        'D' => 
-        array (
-            'Denpa\\Bitcoin\\' => 14,
+            'Psr\\Log\\' => 8,
         ),
         'C' => 
         array (
@@ -37,29 +18,9 @@ class ComposerStaticInit77eafbbc4e8d8d8e07e585a9c1feff8c
     );
 
     public static $prefixDirsPsr4 = array (
-        'Psr\\Http\\Message\\' => 
+        'Psr\\Log\\' => 
         array (
-            0 => __DIR__ . '/..' . '/psr/http-message/src',
-        ),
-        'Psr\\Http\\Client\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/psr/http-client/src',
-        ),
-        'GuzzleHttp\\Psr7\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/guzzlehttp/psr7/src',
-        ),
-        'GuzzleHttp\\Promise\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/guzzlehttp/promises/src',
-        ),
-        'GuzzleHttp\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/guzzlehttp/guzzle/src',
-        ),
-        'Denpa\\Bitcoin\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/denpa/php-bitcoinrpc/src',
+            0 => __DIR__ . '/..' . '/psr/log/Psr/Log',
         ),
         'Composer\\Installers\\' => 
         array (
