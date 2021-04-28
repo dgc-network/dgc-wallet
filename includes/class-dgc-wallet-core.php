51a954e53938f8984b092a123a28bd0db404771e
roverchen <rover.k.chen@gmail.com>
2021-04-28 12:22:29 +0800

    $this->jsonrpc

--- includes/class-dgc-wallet-core.php
+++ includes/class-dgc-wallet-core.php
@@ -71,8 +71,7 @@ if ( ! class_exists( 'dgc_Wallet_Core' ) ) {
                 //$debit_amount = array_sum(wp_list_pluck( get_transactions( array( 'user_id' => $this->user_id, 'where' => array( array( 'key' => 'type', 'value' => 'debit' ) ) ) ), 'amount' ) );
                 //$balance = $credit_amount - $debit_amount;
                 $this->init();
-                $balance = $this->dgc_client->getbalance($this->user_id);
-                //$balance = $this->init();
+                //$balance = $this->dgc_client->getbalance($this->user_id);
                 $this->wallet_balance = apply_filters( 'dgc_wallet_current_balance', $balance, $this->user_id );
             }
             return 'view' === $context ? wc_price( $this->wallet_balance, dgc_wallet_wc_price_args($this->user_id) ) : number_format( $this->wallet_balance, wc_get_price_decimals(), '.', '' );
