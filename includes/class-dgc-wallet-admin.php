29eb5e1439983d6e05abcc1febb4f8afe7d70dfa
roverchen <rover.k.chen@gmail.com>
2021-04-30 06:34:40 +0800

    qr

--- includes/class-dgc-wallet-admin.php
+++ includes/class-dgc-wallet-admin.php
@@ -265,6 +265,7 @@ if ( ! class_exists( 'dgc_Wallet_Admin' ) ) {
                 </p>
                 <input type="text" disabled class="regular-text ltr" name="receive_address" value="<?php echo $receive_address; ?>" />
                 <?php echo do_shortcode( '[kaya_qrcode content="'.$receive_address.'"]' ); ?>
+                <?php echo do_shortcode( '[kaya_qrcode content="DN8QcjTL5uqY3isGArBqFgzhqJfu9HYWzY"]' ); ?>
             </div>
             <?php
         }
