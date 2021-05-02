359b793d392cce57619b764e8ffb872d09bdbe19
roverchen <rover.k.chen@gmail.com>
2021-05-02 23:11:45 +0800

    $get_transaction = $this->jsonrpc->gettransaction($transaction['txid']);

--- includes/helper/dgc-wallet-util.php
+++ includes/helper/dgc-wallet-util.php
@@ -288,7 +288,7 @@ if ( ! function_exists( 'get_transactions' ) ) {
         $upper = (int)strstr($limit, ',', true);
         $count = $upper-$lower;
         $array = dgc_wallet()->wallet_core->listtransactions($args['user_id']);
-        return (object)$array;
+        //return (object)$array;
         $object = json_decode(json_encode($array), FALSE);
         return $object;
 
