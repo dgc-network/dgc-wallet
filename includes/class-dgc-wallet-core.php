22eeae2cd98da1f946c60af3688310d76956bb01
roverchen <rover.k.chen@gmail.com>
2021-05-08 10:43:37 +0800

    sendtoaddress()

--- includes/class-dgc-wallet-core.php
+++ includes/class-dgc-wallet-core.php
@@ -41,7 +41,9 @@ if ( ! class_exists( 'dgc_Wallet_Core' ) ) {
             $rpc_user = dgc_wallet()->settings_api->get_option( 'bitcoind_rpc_username', '_wallet_settings_conf' );
             $rpc_pass = dgc_wallet()->settings_api->get_option( 'bitcoind_rpc_password', '_wallet_settings_conf' );
             $passphrase = dgc_wallet()->settings_api->get_option( 'wallet_passphrase', '_wallet_settings_conf' );
-			$this->jsonrpc = new jsonRPCClient('http://'.$rpc_user.':'.$rpc_pass.'@'.$rpc_host.':'.$rpc_port.'/');
+			if !($this->jsonrpc = new jsonRPCClient('http://'.$rpc_user.':'.$rpc_pass.'@'.$rpc_host.':'.$rpc_port.'/')) {
+                return false;
+            };
         }
 
         function getnewaddress( $user_id = '' ) {
