9f4e3102933b7577a9d1e8eb0e331ca980507ed1
roverchen <rover.k.chen@gmail.com>
2021-05-09 06:37:25 +0800

    //catch(Exception $e) {                         //echo 'Message: ' .$e->getMessage();                     //    throw new Exception('Message: ' .$e->getMessage());                     //}

--- includes/class-dgc-wallet-core.php
+++ includes/class-dgc-wallet-core.php
@@ -40,7 +40,7 @@ if ( ! class_exists( 'dgc_Wallet_Core' ) ) {
             $rpc_pass = dgc_wallet()->settings_api->get_option( 'bitcoind_rpc_password', '_wallet_settings_conf' );
             $passphrase = dgc_wallet()->settings_api->get_option( 'wallet_passphrase', '_wallet_settings_conf' );
             try {
-                $this->jsonrpc = new jsonRPCClient('http://'.$rpc_user.':'.$rpc_pass.'@'.$rpc_host.':'.$rpc_port.'/');
+                //$this->jsonrpc = new jsonRPCClient('http://'.$rpc_user.':'.$rpc_pass.'@'.$rpc_host.':'.$rpc_port.'/');
             }
             catch(Exception $e) {
                 echo 'Message: ' .$e->getMessage();
