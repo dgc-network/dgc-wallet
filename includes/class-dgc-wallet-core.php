0ac8d3e5e2547634f54f14a1c18cb42ea8df3506
roverchen <rover.k.chen@gmail.com>
2021-05-08 17:34:15 +0800

    if (property_exists($response, 'result')) {          echo $response->result;      } elseif (property_exists($response, 'error')) {          echo $response->error->message;      } else {          echo print_r($response);      }

--- includes/class-dgc-wallet-core.php
+++ includes/class-dgc-wallet-core.php
@@ -33,6 +33,12 @@ if ( ! class_exists( 'dgc_Wallet_Core' ) ) {
          */
         public function __construct() {
             $this->user_id = get_current_user_id();
+            $rpc_host = dgc_wallet()->settings_api->get_option( 'bitcoind_rpc_host', '_wallet_settings_conf' );
+            $rpc_port = dgc_wallet()->settings_api->get_option( 'bitcoind_rpc_port', '_wallet_settings_conf' );
+            $rpc_user = dgc_wallet()->settings_api->get_option( 'bitcoind_rpc_username', '_wallet_settings_conf' );
+            $rpc_pass = dgc_wallet()->settings_api->get_option( 'bitcoind_rpc_password', '_wallet_settings_conf' );
+            $passphrase = dgc_wallet()->settings_api->get_option( 'wallet_passphrase', '_wallet_settings_conf' );
+            $this->jsonrpc = new jsonRPCClient('http://'.$rpc_user.':'.$rpc_pass.'@'.$rpc_host.':'.$rpc_port.'/');
         }
 
         function init_rpc() {
@@ -48,12 +54,21 @@ if ( ! class_exists( 'dgc_Wallet_Core' ) ) {
             $addresses = array();
             if ( $user_id != '') {
                 //$this->init_rpc();
-                if ($this->init_rpc())
-                    wp_die(__('You do not have sufficient permissions to access this page.'));
+                //if ($this->init_rpc())
+                //    wp_die(__('You do not have sufficient permissions to access this page.'));
                 $receive_address = get_user_meta( $user_id, 'receive_address' , true );
                 $change_address = get_user_meta( $user_id, 'change_address' , true );
                 if ($receive_address=='') {
                     $receive_address = $this->jsonrpc->getnewaddress();
+/*
+                    if (property_exists($response, 'result')) {
+                        echo $response->result;
+                    } elseif (property_exists($response, 'error')) {
+                        echo $response->error->message;
+                    } else {
+                        echo print_r($response);
+                    }        
+*/
                     update_user_meta( $user_id, 'receive_address' , $receive_address );
                 }
                 if ($change_address=='') {
--- includes/helper/jsonRPCClient.php
+++ includes/helper/jsonRPCClient.php
@@ -153,8 +153,7 @@ class jsonRPCClient {
 			}
 			if (!is_null($response['error'])) {
 				throw new Exception('Request error: '.$response['error']);
-			}
-			
+			}			
 			return $response['result'];
 			
 		} else {
