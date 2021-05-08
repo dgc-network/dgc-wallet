9a8b449f11affffdae400f901017da71d8664118
roverchen <rover.k.chen@gmail.com>
2021-05-09 06:35:52 +0800

    //catch(Exception $e) {                         //echo 'Message: ' .$e->getMessage();                     //    throw new Exception('Message: ' .$e->getMessage());                     //}

--- includes/class-dgc-wallet-core.php
+++ includes/class-dgc-wallet-core.php
@@ -33,14 +33,21 @@ if ( ! class_exists( 'dgc_Wallet_Core' ) ) {
          */
         public function __construct() {
             $this->user_id = get_current_user_id();
-/*
+
             $rpc_host = dgc_wallet()->settings_api->get_option( 'bitcoind_rpc_host', '_wallet_settings_conf' );
             $rpc_port = dgc_wallet()->settings_api->get_option( 'bitcoind_rpc_port', '_wallet_settings_conf' );
             $rpc_user = dgc_wallet()->settings_api->get_option( 'bitcoind_rpc_username', '_wallet_settings_conf' );
             $rpc_pass = dgc_wallet()->settings_api->get_option( 'bitcoind_rpc_password', '_wallet_settings_conf' );
             $passphrase = dgc_wallet()->settings_api->get_option( 'wallet_passphrase', '_wallet_settings_conf' );
-            $this->jsonrpc = new jsonRPCClient('http://'.$rpc_user.':'.$rpc_pass.'@'.$rpc_host.':'.$rpc_port.'/');
-*/            
+            try {
+                $this->jsonrpc = new jsonRPCClient('http://'.$rpc_user.':'.$rpc_pass.'@'.$rpc_host.':'.$rpc_port.'/');
+            }
+            catch(Exception $e) {
+                echo 'Message: ' .$e->getMessage();
+                    //    throw new Exception('Message: ' .$e->getMessage());
+            }
+            //$this->jsonrpc = new jsonRPCClient('http://'.$rpc_user.':'.$rpc_pass.'@'.$rpc_host.':'.$rpc_port.'/');
+
         }
 
         public function get_addresses( $user_id = '' ) {
