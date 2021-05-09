16037e53277fcf834261e027a7c65aa0f73c3889
roverchen <rover.k.chen@gmail.com>
2021-05-09 10:45:00 +0800

    get_option

--- includes/class-dgc-wallet-core.php
+++ includes/class-dgc-wallet-core.php
@@ -37,7 +37,7 @@ if ( ! class_exists( 'dgc_Wallet_Core' ) ) {
 
         function init_rpc() {
 
-            $rpc_host = dgc_wallet()->settings_api->get_option( 'bitcoind_rpc_host', '_wallet_settings_conf' );
+            //$rpc_host = dgc_wallet()->settings_api->get_option( 'bitcoind_rpc_host', '_wallet_settings_conf' );
             //$rpc_port = dgc_wallet()->settings_api->get_option( 'bitcoind_rpc_port', '_wallet_settings_conf' );
             //$rpc_user = dgc_wallet()->settings_api->get_option( 'bitcoind_rpc_username', '_wallet_settings_conf' );
             //$rpc_pass = dgc_wallet()->settings_api->get_option( 'bitcoind_rpc_password', '_wallet_settings_conf' );
