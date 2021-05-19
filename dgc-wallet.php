aa5cca4c782c156cb5168dfa62b544ffa78ba47d
roverchen <rover.k.chen@gmail.com>
2021-05-19 10:09:30 +0800

    include_once dirname( __FILE__ ) . '/wp-bitcoin-wallet/wallets-litecoin-adapter.php';

--- dgc-wallet.php
+++ dgc-wallet.php
@@ -34,7 +34,7 @@ if ( ! defined( 'DGC_WALLET_PLUGIN_FILE' ) ) {
 include_once dirname( __FILE__ ) . '/wp-bitcoin-wallet/widget.php';
 
 add_action( 'wallets_declare_adapters', 'action_wallets_declare_adapters' );
-public function action_wallets_declare_adapters() {
+function action_wallets_declare_adapters() {
 	//include_once __DIR__ . '/includes/wallets-litecoin-adapter.php';
 	include_once dirname( __FILE__ ) . '/wp-bitcoin-wallet/wallets-litecoin-adapter.php';
 }
