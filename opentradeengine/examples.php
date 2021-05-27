cf0ef1b29df9adac7722cb881c649b60255b3088
roverchen <rover.k.chen@gmail.com>
2021-05-27 17:57:04 +0800

    $export_value

--- opentradeengine/examples.php
+++ opentradeengine/examples.php
@@ -23,7 +23,7 @@ function display() {
     handle_post();
     //tab_deposits_cb();
     $email_address = 'rover.k.chen@gmail.com';
-    address_exporter( $email_address, $page = 1 );
+    //address_exporter( $email_address, $page = 1 );
     $export_value = apply_filters( 'wallets_address_exporter', $email_address);
     foreach ($export_value as $data) {
         echo $data;
