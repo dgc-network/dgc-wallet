b0cdd2a8e37610ac8c82223e605c34e3af2705d6
roverchen <rover.k.chen@gmail.com>
2021-05-30 11:01:56 +0800

    $_REQUEST['to_numcoins']

--- opentradeengine/examples.php
+++ opentradeengine/examples.php
@@ -69,7 +69,7 @@ function display() {
             <td><input name="wpbw_widget_send" type="submit" value="Buy" />
             </td>
             <td><?php
-            echo '<input name="to_numcoins" type="text" value="'.$t.'" />';
+            echo '<input name="to_numcoins" type="text" value="'.$_REQUEST["to_numcoins"].'" />';
             ?></td>
         </tr>
     </table>
