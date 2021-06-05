4c6b9bb395cac29f1bdbce98d4a64000c80349e9
roverchen <rover.k.chen@gmail.com>
2021-06-05 12:47:52 +0800

    /*

--- opentradeengine/examples.php
+++ opentradeengine/examples.php
@@ -205,7 +205,8 @@ function handle_post() {
 
 		// check coin already exists
 		$check_coin_exists = $wpdb->get_var( $wpdb->prepare( " select id from {$wapg_tables['coins']} where name = '%s' and checkout_type = %d ", $coin_info['coin_name'], $coin_info['checkout_type'] ) );
-		if ( $check_coin_exists ) {
+/*
+        if ( $check_coin_exists ) {
 			wp_send_json(
 				array(
 					'status' => false,
@@ -214,7 +215,7 @@ function handle_post() {
 				)
 			);
 		}
-/*
+
 		if ( empty( $coin_web_id = get_coin_id( $coin_info['coin_name'], $coin_info['checkout_type'] ) ) ) {
 			wp_send_json(
 				array(
